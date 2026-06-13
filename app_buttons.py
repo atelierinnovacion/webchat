af82bdbd588a2323d08bd4ecb2dcfb3b4f5be14f
atelierinnovacion <atelier.innovacion@gmail.com>
2026-06-13 11:05:37 -0400

    Update app_buttons.py

--- app_buttons.py
+++ app_buttons.py
@@ -150,10 +150,10 @@ if final_input:
         unanswered_question = st.session_state.waiting_for_email
         
         # Google Form Integration
-        FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfXXXXXXXXXXXXX/formResponse" # Replace with your real URL
+        FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLScWL62CTOoP_dRlw_XhU2Wgd6IdA9P6vhmvXRT5XXG3HiUNNg/formResponsee" # Replace with your real URL
         form_data = {
-            "entry.123456789": user_email,          # Replace with your Email ID
-            "entry.987654321": unanswered_question   # Replace with your Question ID
+            "entry.1621915346": user_email,          # Replace with your Email ID
+            "entry.409076594": unanswered_question   # Replace with your Question ID
         }
         try:
             requests.post(FORM_URL, data=form_data, timeout=5)
